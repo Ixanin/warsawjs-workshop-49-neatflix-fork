791e10dc98f7c242bb849390fa2b20a97cc50868
Krzysztof Trekiel <krzysztof.trekiel@deviniti.com>
2020-10-18 14:07:14 +0200

    zad 4/5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <ShowsListItem />
+    <ShowsListItem
+        :show="show"
+        :showSummary="showSummary"
+        v-on:showDetails="showDetails"
+    />
   </div>
 </template>
 ------------------------------------------------------
@@ -12,11 +16,26 @@ export default {
   components: {
     ShowsListItem
   },
-  // methods: {},
+  methods: {
+      showDetails() {
+          this.showSummary = !this.showSummary;
+      }
+  },
   // computed: {},
   // watch: {},
 
-  // data() {},
+    data() {
+        return {
+            showSummary: false,
+            show: {
+                name: 'Kipo and the Age of Wonderbeasts',
+                premiered: 2019, // rok premiery
+                rating: 10, // ocena, 0 - 10
+                imgURL: 'https://image.tmdb.org/t/p/original/v7i8qBl5WKJHEh0z0omAdGwvso7.jpg',
+                summary: 'Kipo, a sheltered girl, gets a crash course in survival when a mutant attack sends her to the surface, far from the safety of her underground home.'
+            },
+        }
+    },
   // created() {},
   // destroyed() {},
 }
--- src/components/ShowsListItem.vue
+++ src/components/ShowsListItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div
       class="shows-list-item"
-      v-on:click="onClick"
       :style="{backgroundImage: `url(${show.imgURL})`}"
+      v-on:click="$emit('showDetails')"
   >
     <div class="info-wrapper">
       <div v-if="!showSummary">
@@ -20,18 +20,7 @@
 <script>
     export default {
         name: 'ShowsListItem',
-        data() {
-            return {
-                showSummary: false,
-                show: {
-                    name: 'Kipo and the Age of Wonderbeasts',
-                    premiered: 2019, // rok premiery
-                    rating: 10, // ocena, 0 - 10
-                    imgURL: 'https://image.tmdb.org/t/p/original/v7i8qBl5WKJHEh0z0omAdGwvso7.jpg',
-                    summary: 'Kipo, a sheltered girl, gets a crash course in survival when a mutant attack sends her to the surface, far from the safety of her underground home.'
-                },
-            }
-        },
+        props: ['show', 'showSummary'],
         methods: {
             onClick() {
                 this.showSummary = !this.showSummary;
